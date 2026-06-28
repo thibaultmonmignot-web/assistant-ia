f1b99b8c614da0750df5c1bd415cf5ed69e1a47e
thibaultmonmignot-web <thibaultmonmignot@gmail.com>
2026-06-28 12:04:07 +0200

    Update memoire.js

--- memoire.js
+++ memoire.js
@@ -1,1 +1,21 @@
+// =========================
+// Mémoire de Toban AI
+// =========================
 
+function enregistrer(cle, valeur){
+
+    localStorage.setItem(cle, valeur);
+
+}
+
+function lire(cle){
+
+    return localStorage.getItem(cle);
+
+}
+
+function oublier(cle){
+
+    localStorage.removeItem(cle);
+
+}
